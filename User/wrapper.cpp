64d22ffb5c7483ec0862186f4b798404f9baafd1
Kase, Misao <29609700+pic-man749@users.noreply.github.com>
2025-02-04 21:31:00 +0900

    initial commit

---
+++ User/wrapper.cpp
@@ -0,0 +1,16 @@
+/*
+ * wrapper.cpp
+ *
+ *  Created on: Dec 1, 2024
+ *      Author: picman
+ */
+
+#include "wrapper.hpp"
+
+void setup(void){
+
+}
+
+void loop(void){
+
+}
---
+++ User/wrapper.hpp
@@ -0,0 +1,24 @@
+/*
+ * wrapper.hpp
+ *
+ *  Created on: Dec 1, 2024
+ *      Author: picman
+ */
+
+#ifndef _WRAPPER_INCLUDE_HPP_
+#define _WRAPPER_INCLUDE_HPP_
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void setup(void);
+void loop(void);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
