840bca271e24d9b9dab1db5f40b85dfb879f7e33
Kase, Misao <29609700+pic-man749@users.noreply.github.com>
2025-02-15 13:05:52 +0900

    [making]

--- User/App/Screen/ScreenFreq.cpp
+++ User/App/Screen/ScreenFreq.cpp
@@ -11,13 +11,84 @@
 
 namespace App {
 
+  namespace {
+
+    int intpow(int base, uint32_t exp) {
+      int ret = 0;
+      if(exp == 0){
+        ret = 1;
+      } else {
+        ret = base;
+        for(uint32_t i = 1; i < exp; ++i){
+          ret *= base;
+        }
+      }
+      return ret;
+    }
+
+  }
+
+  ScreenFreq::ScreenFreq(void) {
+    ;
+  }
+
   std::unique_ptr<IScreen> ScreenFreq::StartScreen(){
 
     PrintLabelLine1();
     PrintLabelLine2();
     PrintFreq();
 
+    auto lcd = LCD::Instance();
+    lcd->SetCursorPos(2, Digit2Pos[_selectedDigit.GetNowPos()]);
+    lcd->SetCursorDisp(true);
+
     return nullptr;
+  }
+
+  std::unique_ptr<IScreen> ScreenFreq::LeftButtonPushed(void) {
+    uint8_t digit = _selectedDigit.MoveLeft();
+    auto lcd = LCD::Instance();
+    lcd->SetCursorPos(2, Digit2Pos[digit]);
+    return nullptr;
+  }
+
+  std::unique_ptr<IScreen> ScreenFreq::RightButtonPushed(void) {
+    uint8_t digit = _selectedDigit.MoveRight();
+    auto lcd = LCD::Instance();
+    lcd->SetCursorPos(2, Digit2Pos[digit]);
+    return nullptr;
+  }
+
+  std::unique_ptr<IScreen> ScreenFreq::CenterButtonPushed(void) {
+    return nullptr;
+  }
+
+  std::unique_ptr<IScreen> ScreenFreq::EncoderTurned(int step) {
+    auto ao = AnalogOut::Instance();
+    auto lcd = LCD::Instance();
+
+    int diff = step * intpow(10, _selectedDigit.GetNowPos());
+
+    uint32_t nowFreq = ao->GetFreqx10();
+    uint32_t newFreq = 0;
+    if((int)nowFreq + diff > 0) {
+      newFreq = (uint32_t)((int)nowFreq + diff);
+    }
+
+    ao->SetFreqx10(newFreq);
+
+    PrintFreq();
+
+    uint8_t digit = _selectedDigit.GetNowPos();
+    lcd->SetCursorPos(2, Digit2Pos[digit]);
+
+    return nullptr;
+  }
+
+  void ScreenFreq::EndScreen(void) {
+    auto lcd = LCD::Instance();
+    lcd->SetCursorDisp(false);
+    lcd->ClearDisp();
   }
 
   void ScreenFreq::PrintLabelLine1(void){
@@ -44,7 +115,6 @@ namespace App {
   bool ScreenFreq::PrintFreq(void){
 
     const int BUFFER_SIZE = 16;
-    const int MAX_FREQ_NUM_OF_DIGITS = 8;
 
     auto lcd = LCD::Instance();
     auto ao = AnalogOut::Instance();
@@ -81,6 +151,24 @@ namespace App {
     lcd->sputs(lcdBuffer);
 
     return true;
+  }
+
+  void ScreenFreq::SetCursorPos(Direction d) {
+    uint8_t digit = _selectedDigit.GetNowPos();
+    switch (d) {
+      case Direction::LEFT:
+        digit = _selectedDigit.MoveLeft();
+        break;
+      case Direction::RIGHT:
+        digit = _selectedDigit.MoveRight();
+        break;
+      default:
+        ;
+        break;
+    }
+
+    LCD::Instance()->SetCursorPos(2, Digit2Pos[digit]);
+
   }
 
 }
