5419640f9386602f403a9cdde68431edc1553693
Kase, Misao <29609700+pic-man749@users.noreply.github.com>
2025-02-24 11:15:38 +0900

    [add]doc comment

--- User/App/IScreen.hpp
+++ User/App/IScreen.hpp
@@ -15,6 +15,10 @@
 
 namespace App{
 
+  /// @class IScreen
+  /// @brief Interface of Screen class.
+  /// @brief All screens inherit and implement this interface.
+  ///
   class IScreen {
 
     public:
@@ -22,20 +26,59 @@ namespace App{
 
       virtual ~IScreen(void);
 
+      /// @fn std::unique_ptr<IScreen> StartScreen(void)
+      /// @brief Callback of screen start.
+      ///
+      /// @return nullptr continue now screen
+      /// @return other next screen
       virtual std::unique_ptr<IScreen> StartScreen(void);
 
+      /// @fn std::unique_ptr<IScreen> LeftButtonPushed(void)
+      /// @brief Callback of left button pushed.
+      ///
+      /// @return nullptr continue now screen
+      /// @return other next screen
       virtual std::unique_ptr<IScreen> LeftButtonPushed(void);
 
+      /// @fn std::unique_ptr<IScreen> BottomButtonPushed(void)
+      /// @brief Callback of bottom button pushed.
+      ///
+      /// @return nullptr continue now screen
+      /// @return other next screen
       virtual std::unique_ptr<IScreen> BottomButtonPushed(void);
 
+      /// @fn std::unique_ptr<IScreen> TopButtonPushed(void)
+      /// @brief Callback of top button pushed.
+      ///
+      /// @return nullptr continue now screen
+      /// @return other next screen
       virtual std::unique_ptr<IScreen> TopButtonPushed(void);
 
+      /// @fn std::unique_ptr<IScreen> RightButtonPushed(void)
+      /// @brief Callback of right button pushed.
+      ///
+      /// @return nullptr continue now screen
+      /// @return other next screen
       virtual std::unique_ptr<IScreen> RightButtonPushed(void);
 
+      /// @fn std::unique_ptr<IScreen> CenterButtonPushed(void)
+      /// @brief Callback of center button pushed.
+      ///
+      /// @return nullptr continue now screen
+      /// @return other next screen
       virtual std::unique_ptr<IScreen> CenterButtonPushed(void);
 
+      /// @fn std::unique_ptr<IScreen> EncoderTurned(int)
+      /// @brief Callback of encoder turned.
+      ///
+      /// @param step turned amount, +:CW, -:CCW
+      /// @return nullptr continue now screen
+      /// @return other next screen
       virtual std::unique_ptr<IScreen> EncoderTurned(int step);
 
+      /// @fn void EndScreen(void)
+      /// @brief Interface of Screen class that called screen end.
+      ///
       virtual void EndScreen(void);
 
   };
